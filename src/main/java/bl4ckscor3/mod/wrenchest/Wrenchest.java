11fe151f9f60a9adfc24bf998e4822708d5ae698
bl4ckscor3 <donalddani9612@gmx.de>
2019-06-13 13:46:13 +0200

    update to 1.14.2

--- src/main/java/bl4ckscor3/mod/wrenchest/Wrenchest.java
+++ src/main/java/bl4ckscor3/mod/wrenchest/Wrenchest.java
@@ -1,15 +1,15 @@
 package bl4ckscor3.mod.wrenchest;
 
-import net.minecraft.block.BlockChest;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Items;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.Items;
 import net.minecraft.state.properties.ChestType;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -36,12 +36,12 @@ public class Wrenchest
 			}
 
 			@Override
-			public EnumActionResult onItemUseFirst(ItemStack stack, ItemUseContext ctx)
+			public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext ctx)
 			{
-				EnumActionResult result = checkConnections(stack, ctx);
+				ActionResultType result = checkConnections(stack, ctx);
 
-				if(result == EnumActionResult.SUCCESS && !ctx.getPlayer().isCreative())
-					stack.damageItem(1, ctx.getPlayer());
+				if(result == ActionResultType.SUCCESS && !ctx.getPlayer().isCreative())
+					stack.damageItem(1, ctx.getPlayer(), p -> {});
 
 				return result;
 			}
@@ -50,65 +50,65 @@ public class Wrenchest
 			 * Checks which way two chests might be facing each other and then connects them
 			 * @see Item#onItemUseFirst
 			 */
-			private EnumActionResult checkConnections(ItemStack stack, ItemUseContext ctx)
+			private ActionResultType checkConnections(ItemStack stack, ItemUseContext ctx)
 			{
-				if(!(ctx.getWorld().getBlockState(ctx.getPos()).getBlock() instanceof BlockChest))
-					return EnumActionResult.PASS;
+				if(!(ctx.getWorld().getBlockState(ctx.getPos()).getBlock() instanceof ChestBlock))
+					return ActionResultType.PASS;
 
 				World world = ctx.getWorld();
 				BlockPos pos = ctx.getPos();
-				IBlockState chestState = world.getBlockState(pos);
+				BlockState chestState = world.getBlockState(pos);
 
-				if(chestState.get(BlockChest.TYPE) != ChestType.SINGLE) //disconnect double chests
+				if(chestState.get(ChestBlock.TYPE) != ChestType.SINGLE) //disconnect double chests
 				{
-					EnumFacing facingTowardsOther = BlockChest.getDirectionToAttached(chestState);
+					Direction facingTowardsOther = ChestBlock.getDirectionToAttached(chestState);
 
-					world.setBlockState(pos, chestState.with(BlockChest.TYPE, ChestType.SINGLE));
-					world.setBlockState(pos.offset(facingTowardsOther), world.getBlockState(pos.offset(facingTowardsOther)).with(BlockChest.TYPE, ChestType.SINGLE));
-					return EnumActionResult.SUCCESS;
+					world.setBlockState(pos, chestState.with(ChestBlock.TYPE, ChestType.SINGLE));
+					world.setBlockState(pos.offset(facingTowardsOther), world.getBlockState(pos.offset(facingTowardsOther)).with(ChestBlock.TYPE, ChestType.SINGLE));
+					return ActionResultType.SUCCESS;
 				}
-				else if(ctx.getFace() != EnumFacing.UP && ctx.getFace() != EnumFacing.DOWN) //connect single chests, UP/DOWN check is here so double chests can be disconected by clicking on all faces
+				else if(ctx.getFace() != Direction.UP && ctx.getFace() != Direction.DOWN) //connect single chests, UP/DOWN check is here so double chests can be disconected by clicking on all faces
 				{
 					BlockPos otherPos = pos.offset(ctx.getFace());
-					IBlockState otherState = world.getBlockState(otherPos);
+					BlockState otherState = world.getBlockState(otherPos);
 
-					if(otherState.getBlock() instanceof BlockChest && otherState.get(BlockChest.TYPE) == ChestType.SINGLE)
+					if(otherState.getBlock() instanceof ChestBlock && otherState.get(ChestBlock.TYPE) == ChestType.SINGLE)
 					{
-						EnumFacing facing = chestState.get(BlockChest.FACING);
-						EnumFacing otherFacing = otherState.get(BlockChest.FACING);
+						Direction facing = chestState.get(ChestBlock.FACING);
+						Direction otherFacing = otherState.get(ChestBlock.FACING);
 
 						//the chests are facing (away from) each other
 						if((ctx.getFace() == facing || ctx.getFace() == otherFacing) && facing.getOpposite() == otherFacing)
 						{
-							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), facing, ctx.getHitX(), ctx.getHitZ(), true))
-								return EnumActionResult.SUCCESS;
-							else return EnumActionResult.PASS;
+							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), facing, ctx.func_221532_j().x % 1, ctx.func_221532_j().z % 1, true))
+								return ActionResultType.SUCCESS;
+							else return ActionResultType.PASS;
 						}
 						//the clicked chest has the other chest to its left/right
 						else if(ctx.getFace().rotateY() == facing || ctx.getFace().rotateYCCW() == facing)
 						{
-							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), facing, ctx.getHitX(), ctx.getHitZ(), false))
-								return EnumActionResult.SUCCESS;
-							else return EnumActionResult.PASS;
+							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), facing, ctx.func_221532_j().x % 1, ctx.func_221532_j().z % 1, false))
+								return ActionResultType.SUCCESS;
+							else return ActionResultType.PASS;
 						}
 						//the clicked chest has its neighbor to the front/back
 						else if(ctx.getFace().rotateY() == otherFacing || ctx.getFace().rotateYCCW() == otherFacing)
 						{
-							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), otherFacing, ctx.getHitX(), ctx.getHitZ(), false))
-								return EnumActionResult.SUCCESS;
-							else return EnumActionResult.PASS;
+							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), otherFacing, ctx.func_221532_j().x % 1, ctx.func_221532_j().z % 1, false))
+								return ActionResultType.SUCCESS;
+							else return ActionResultType.PASS;
 						}
 						//the chests are facing in the same direction, but are placed behind each other. the case where they are standing next to each other facing the same direction is covered before
 						else if(facing == otherFacing)
 						{
-							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), facing.rotateY(), ctx.getHitX(), ctx.getHitZ(), false))
-								return EnumActionResult.SUCCESS;
-							else return EnumActionResult.PASS;
+							if(connectChests(world, pos, otherPos, chestState, otherState, ctx.getFace(), facing.rotateY(), ctx.func_221532_j().x % 1, ctx.func_221532_j().z % 1, false))
+								return ActionResultType.SUCCESS;
+							else return ActionResultType.PASS;
 						}
 					}
 				}
 
-				return EnumActionResult.PASS;
+				return ActionResultType.PASS;
 			}
 
 			/**
@@ -125,31 +125,31 @@ public class Wrenchest
 			 * @param swapDirections Whether to swap the directions to check the hit data on
 			 * @return true if the chests were connected, false otherwise
 			 */
-			private boolean connectChests(World world, BlockPos clickedPos, BlockPos otherPos, IBlockState clickedState, IBlockState otherState, EnumFacing clickedFace, EnumFacing chestFacing, float hitX, float hitZ, boolean swapDirections)
+			private boolean connectChests(World world, BlockPos clickedPos, BlockPos otherPos, BlockState clickedState, BlockState otherState, Direction clickedFace, Direction chestFacing, double hitX, double hitZ, boolean swapDirections)
 			{
-				EnumFacing newFacing = EnumFacing.UP;
+				Direction newFacing = Direction.UP;
 
 				if(!swapDirections)
 				{
-					if(chestFacing == EnumFacing.NORTH || chestFacing == EnumFacing.SOUTH)
-						newFacing = hitZ < 0.5 ? EnumFacing.NORTH : EnumFacing.SOUTH;
-					else if(chestFacing == EnumFacing.WEST || chestFacing == EnumFacing.EAST)
-						newFacing = hitX < 0.5 ? EnumFacing.WEST : EnumFacing.EAST;
+					if(chestFacing == Direction.NORTH || chestFacing == Direction.SOUTH)
+						newFacing = hitZ < 0.5 ? Direction.NORTH : Direction.SOUTH;
+					else if(chestFacing == Direction.WEST || chestFacing == Direction.EAST)
+						newFacing = hitX < 0.5 ? Direction.WEST : Direction.EAST;
 				}
 				else
 				{
-					if(chestFacing == EnumFacing.WEST || chestFacing == EnumFacing.EAST)
-						newFacing = hitZ < 0.5 ? EnumFacing.NORTH : EnumFacing.SOUTH;
-					else if(chestFacing == EnumFacing.NORTH || chestFacing == EnumFacing.SOUTH)
-						newFacing = hitX < 0.5 ? EnumFacing.WEST : EnumFacing.EAST;
+					if(chestFacing == Direction.WEST || chestFacing == Direction.EAST)
+						newFacing = hitZ < 0.5 ? Direction.NORTH : Direction.SOUTH;
+					else if(chestFacing == Direction.NORTH || chestFacing == Direction.SOUTH)
+						newFacing = hitX < 0.5 ? Direction.WEST : Direction.EAST;
 				}
 
-				if(newFacing != EnumFacing.UP)
+				if(newFacing != Direction.UP)
 				{
 					ChestType newType = getNewChestType(clickedFace, newFacing);
 
-					world.setBlockState(clickedPos, clickedState.with(BlockChest.FACING, newFacing).with(BlockChest.TYPE, newType));
-					world.setBlockState(otherPos, otherState.with(BlockChest.FACING, newFacing).with(BlockChest.TYPE, newType.opposite()));
+					world.setBlockState(clickedPos, clickedState.with(ChestBlock.FACING, newFacing).with(ChestBlock.TYPE, newType));
+					world.setBlockState(otherPos, otherState.with(ChestBlock.FACING, newFacing).with(ChestBlock.TYPE, newType.opposite()));
 					return true;
 				}
 
@@ -162,14 +162,14 @@ public class Wrenchest
 			 * @param chestFacing The facing of the chest that was clicked
 			 * @return The new chest type for the clicked chest
 			 */
-			private ChestType getNewChestType(EnumFacing clickedFace, EnumFacing chestFacing)
+			private ChestType getNewChestType(Direction clickedFace, Direction chestFacing)
 			{
 				switch(chestFacing) //figure out whether the clicked chest will connect to the left or right
 				{
-					case NORTH: return clickedFace == EnumFacing.WEST ? ChestType.RIGHT : ChestType.LEFT;
-					case SOUTH: return clickedFace == EnumFacing.WEST ? ChestType.LEFT : ChestType.RIGHT;
-					case EAST: return clickedFace == EnumFacing.NORTH ? ChestType.RIGHT : ChestType.LEFT;
-					case WEST: return clickedFace == EnumFacing.NORTH ? ChestType.LEFT : ChestType.RIGHT;
+					case NORTH: return clickedFace == Direction.WEST ? ChestType.RIGHT : ChestType.LEFT;
+					case SOUTH: return clickedFace == Direction.WEST ? ChestType.LEFT : ChestType.RIGHT;
+					case EAST: return clickedFace == Direction.NORTH ? ChestType.RIGHT : ChestType.LEFT;
+					case WEST: return clickedFace == Direction.NORTH ? ChestType.LEFT : ChestType.RIGHT;
 					default: return ChestType.SINGLE;
 				}
 			}
